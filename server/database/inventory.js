08e594f37cdac57175fa4cd80c4e8333f97127e5
knr83 <avtochasty.com.ua@gmail.com>
2024-11-10 12:47:31 -0500

    Models complete

--- server/database/inventory.js
+++ server/database/inventory.js
@@ -4,10 +4,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cars = new Schema({
-dealer_id: {
-    type: Number,
-    required: true
-},
 make: {
     type: String,
     required: true
